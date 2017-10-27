91e9a2fed694e0813592b932d22cce56322f954f
Luis Barreiro <2161323@my.ipleiria.pt>
2017-10-27 18:29:14 +0100

    NEW CONNECTION STRING

--- DBProds/Controllers/ProductsController.cs
+++ DBProds/Controllers/ProductsController.cs
@@ -16,7 +16,7 @@ namespace ProductsAPI.Controllers
         //Probably a database in a real scenario...
         public String getConnectionString()
         {
-            var uriString = ConfigurationManager.AppSettings["4db8c604-d7f7-4f44-a85e-a81900fa0271.sqlserver.sequelizer.com"];
+            /*var uriString = ConfigurationManager.AppSettings["4db8c604-d7f7-4f44-a85e-a81900fa0271.sqlserver.sequelizer.com"];
             var uri = new Uri(uriString);
             var connectionString = new SqlConnectionStringBuilder
             {
@@ -28,6 +28,9 @@ namespace ProductsAPI.Controllers
                 Password = "jYHo42aBzidnUTZ5asAMSrnppLYaYWiPvSyanaMeftE3KDEdAnczTCTZLs8qb8KL",
             }.ConnectionString;
             return connectionString;
+            */
+            string connctionStr = @"Server=4db8c604-d7f7-4f44-a85e-a81900fa0271.sqlserver.sequelizer.com;Database=db4db8c604d7f74f44a85ea81900fa0271;User ID=ycuybrshuiebsfdm;Password=jYHo42aBzidnUTZ5asAMSrnppLYaYWiPvSyanaMeftE3KDEdAnczTCTZLs8qb8KL";
+            return ConnectionStr;
         }
 
         
