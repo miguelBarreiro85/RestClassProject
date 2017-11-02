700781e88c5a540a8c3e0d2b3c9f731fdb5773c3
Luis Barreiro <2161323@my.ipleiria.pt>
2017-11-02 23:49:58 +0000

    add app

---
+++ ClientProductsApp-base/FormProducts.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ClientProductsApp
+{
+    partial class FormProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGetAll = new System.Windows.Forms.Button();
+            this.richTextBoxShowProducts = new System.Windows.Forms.RichTextBox();
+            this.buttonGetProductById = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonPost = new System.Windows.Forms.Button();
+            this.buttonPut = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGetAll
+            // 
+            this.buttonGetAll.Location = new System.Drawing.Point(21, 27);
+            this.buttonGetAll.Name = "buttonGetAll";
+            this.buttonGetAll.Size = new System.Drawing.Size(111, 23);
+            this.buttonGetAll.TabIndex = 0;
+            this.buttonGetAll.Text = "Get All Products";
+            this.buttonGetAll.UseVisualStyleBackColor = true;
+            this.buttonGetAll.Click += new System.EventHandler(this.buttonGetAll_Click);
+            // 
+            // richTextBoxShowProducts
+            // 
+            this.richTextBoxShowProducts.Location = new System.Drawing.Point(21, 56);
+            this.richTextBoxShowProducts.Name = "richTextBoxShowProducts";
+            this.richTextBoxShowProducts.Size = new System.Drawing.Size(318, 121);
+            this.richTextBoxShowProducts.TabIndex = 1;
+            this.richTextBoxShowProducts.Text = "";
+            // 
+            // buttonGetProductById
+            // 
+            this.buttonGetProductById.Location = new System.Drawing.Point(21, 195);
+            this.buttonGetProductById.Name = "buttonGetProductById";
+            this.buttonGetProductById.Size = new System.Drawing.Size(111, 23);
+            this.buttonGetProductById.TabIndex = 2;
+            this.buttonGetProductById.Text = "Get Product {?}";
+            this.buttonGetProductById.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(21, 225);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(318, 56);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(418, 62);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 20);
+            this.textBoxID.TabIndex = 5;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(418, 89);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(240, 20);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(418, 116);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(240, 20);
+            this.textBoxCategory.TabIndex = 7;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(418, 143);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(365, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Category:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(365, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Price:";
+            // 
+            // buttonPost
+            // 
+            this.buttonPost.Location = new System.Drawing.Point(418, 194);
+            this.buttonPost.Name = "buttonPost";
+            this.buttonPost.Size = new System.Drawing.Size(91, 23);
+            this.buttonPost.TabIndex = 13;
+            this.buttonPost.Text = "POST (new)";
+            this.buttonPost.UseVisualStyleBackColor = true;
+            this.buttonPost.Click += new System.EventHandler(this.buttonPost_Click);
+            // 
+            // buttonPut
+            // 
+            this.buttonPut.Location = new System.Drawing.Point(418, 225);
+            this.buttonPut.Name = "buttonPut";
+            this.buttonPut.Size = new System.Drawing.Size(91, 23);
+            this.buttonPut.TabIndex = 14;
+            this.buttonPut.Text = "PUT (Update)";
+            this.buttonPut.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(418, 255);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(91, 23);
+            this.buttonDelete.TabIndex = 15;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // FormProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 338);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonPut);
+            this.Controls.Add(this.buttonPost);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonGetProductById);
+            this.Controls.Add(this.richTextBoxShowProducts);
+            this.Controls.Add(this.buttonGetAll);
+            this.Name = "FormProducts";
+            this.Text = "RESTFull client application - Products";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGetAll;
+        private System.Windows.Forms.RichTextBox richTextBoxShowProducts;
+        private System.Windows.Forms.Button buttonGetProductById;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonPost;
+        private System.Windows.Forms.Button buttonPut;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ ClientProductsApp-base/FormProducts.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO; //Stream
+using System.Linq;
+using System.Net; //HttpWebRequest
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+//JavaScriptSerializer --> necessário criar referencia para System.Web.Extensions
+
+namespace ClientProductsApp
+{
+    public partial class FormProducts : Form
+    {
+
+        public static string baseURI = @"http://restproductslab.apphb.com/api/products"; //needs to be updated!
+        public static HttpClient client = new HttpClient();
+
+    
+    
+        public FormProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetAll_Click(object sender, EventArgs e)
+        {
+
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(baseURI);
+                richTextBoxShowProducts.Text = json;
+            }
+
+        }
+
+        private void buttonPost_Click(object sender, EventArgs e)
+        {
+            Product newProduct = new Product
+            {
+                Name = textBoxName.Text,
+                Category = textBoxCategory.Text,
+                Price = decimal.Parse(textBoxPrice.Text)
+            };
+            CreateProductAsync(newProduct);
+
+        }
+        static async Task RunAsync()
+        {
+            // New code:
+            client.BaseAddress = new Uri("http://restproductslab.apphb.com");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Console.ReadLine();
+        }
+        static async Task<Uri> CreateProductAsync(Product product)
+        {
+            // HttpResponseMessage response = await client.PostAsJsonAsync("api/products", product);
+            // response.EnsureSuccessStatusCode();
+            var response = await client.PostAsync("http://restproductslab.apphb.com/api/products",
+            new StringContent(product.ToString(), Encoding.UTF8, "application/json"));
+            // Return the URI of the created resource.
+            MessageBox.Show(response.Headers.Location.ToString());
+            return response.Headers.Location;
+            
+        }
+    }
+}
